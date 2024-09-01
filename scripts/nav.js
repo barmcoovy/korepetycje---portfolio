d289c4fda6749899829ffe34b23b184e38aeb584
barmc <bartoszmiecznikowski853@gmail.com>
2024-09-01 11:29:06 +0200

    third commit

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,6 +1,24 @@
 const navigation = document.querySelector("#navigation");
 const links = ["O mnie", "Oferta", "Kontakt"];
 
-for(let i =0; i<links.length;i++){
-    navigation.innerHTML +=`<span class='link'><a href="#${links[i].toLowerCase().replace(" ","-")}">${links[i]}</a></span>`;
+for (let i = 0; i < links.length; i++) {
+    const span = document.createElement('span');
+    span.id = `link-${i}`;
+    span.className = 'link';
+
+    const anchor = document.createElement('a');
+    anchor.href = `#${links[i].toLowerCase().replace(" ", "-")}`;
+    anchor.textContent = links[i];
+
+    anchor.addEventListener('click', function (event) {
+        event.preventDefault(); // Zapobiega zmianie URL
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+        if (targetElement) {
+            targetElement.scrollIntoView({ behavior: 'smooth' });
+        }
+    });
+
+    span.appendChild(anchor);
+    navigation.appendChild(span);
 }
